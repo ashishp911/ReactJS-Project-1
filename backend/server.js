2dc7e9412bb2361f2be83a1259d26a0c81cfa814
ashishp911 <ashish.patil35@gmail.com>
2023-02-24 00:04:43 -0500

    coded backend (server.js)

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+// app.use(express.static('../public'))
+app.use(express.json({limit : "1mb"}));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+})
+
+// POST method route (as we are receiving data from the server)
+app.post('/api', (request, response) => {
+    console.log("I got a request");
+    console.log(request.body);
+    response.json({
+        status : "success",
+        msg : "Server got your data"
+    });
+    console.log("Sending data back to client");
 })
